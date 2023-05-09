743099f4243253b6529e074c623bea252b489505
Juan Sebastian Teran <sebasterra69@gmail.com>
2023-05-08 23:23:26 -0600

    recomendaciones vol. 1

--- Sabinito-Smile/src/app/app.module.ts
+++ Sabinito-Smile/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { CitasComponent } from './citas/citas.component';
 import { FooterComponent } from './footer/footer.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { DatePipe } from '@angular/common';
+import { RecoComponent } from './reco/reco.component';
 
 @NgModule({
   declarations: [
@@ -27,6 +28,7 @@ import { DatePipe } from '@angular/common';
     RevisarComponent,
     CitasComponent,
     FooterComponent,
+    RecoComponent,
   ],
   imports: [
     BrowserModule,
