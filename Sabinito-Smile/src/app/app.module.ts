cf94423447a0863cbe26f19598ad2d7bcf8bd367
Juan Manuel González Alaniz <j.mannuelg.a@gmail.com>
2023-05-09 22:56:35 -0600

    Contactanos V1

--- Sabinito-Smile/src/app/app.module.ts
+++ Sabinito-Smile/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
@@ -12,7 +13,7 @@ import { AgendarComponent } from './citas/agendar/agendar.component';
 import { RevisarComponent } from './citas/revisar/revisar.component';
 import { CitasComponent } from './citas/citas.component';
 import { FooterComponent } from './footer/footer.component';
-import { ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule,FormsModule } from '@angular/forms';
 import { DatePipe } from '@angular/common';
 import { RecoComponent } from './reco/reco.component';
 
